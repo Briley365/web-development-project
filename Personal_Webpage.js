92486b5bcd5ebcde78019d00a318b418df172029
Briley365 <bharre12@students.kennesaw.edu>
2024-11-10 17:02:54 -0500

    Add files via upload

--- Personal_Webpage.js
+++ Personal_Webpage.js
@@ -36,3 +36,40 @@ function nextSlide(){
     slideIndex++;
     showSlide(slideIndex);
 }
+//Form validation 
+const form = document.getElementById('form');
+const Name = document.getElementById('Name');
+const Email = document.getElementById('Email');
+const Phone = document.getElementById('Phone');
+const Name_error = document.getElementById('Name_error');
+const Email_error = document.getElementById('Email_error');
+const Phone_error = document.getElementById('Phone_error');
+
+//used to prevent form from submitting
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    //Form validation for Name, Email, and Phone number.
+    var Email_check = /^([A-Za-z0-9_|-|.])+|@([A-Za-z0-9_1-1.])+\ ([A-Za-z]{2,4})$/;
+
+    if(Name.value === "" || Name.value == null){
+        Name_error.innerHTML = "You must submit a name.";
+    }
+    if (!Email.value.match(Email_check)){
+        Email_error.innerHTML = "Valid email is needed. ";
+        return false;
+    }
+    if(Phone.length == 0){
+        Phone_error.innerHTML = 'Phone # is required.' 
+        return false;
+    }
+    if(Phone.value.length != 10){
+        Phone_error.innerHTML = 'Phone # should be 10 digits long.' 
+        return false;
+    }
+    if(!Phone.match(/^[0-9]{10}$/)){
+        Phone_error.innerHTML = 'Phone # is required.' 
+        return false;
+    }
+
+}
+);
